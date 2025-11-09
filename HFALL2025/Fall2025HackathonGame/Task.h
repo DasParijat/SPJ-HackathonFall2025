6d6eee509b31f1ff13ae5fd4a45ef347777a6ff3
smasone <sophia.masone1@marist.edu>
2025-11-09 09:57:34 -0500

    Task header

--- HFALL2025/Fall2025HackathonGame/Task.h
+++ HFALL2025/Fall2025HackathonGame/Task.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <ctime>
+
+class Task {
+
+private:
+	std::string title;
+	struct tm dueDate;
+	int difficulty;
+	bool isCompleted;
+	std::string note;
+
+public:
+	Task(std::string title, int day, int month, int year, int difficulty, std::string note = "");
+	bool complete();
+
+};
